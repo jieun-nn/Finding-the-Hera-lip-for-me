0916bcc9238e0bbb8fe18925b770500709405c94
jieun-nn <hurshey12@naver.com>
2023-04-13 10:54:58 +0900

    헤라 립 틀 제작(콘텐츠X)

---
+++ js/data.js
@@ -0,0 +1,156 @@
+export const questions = [
+  {
+    number: '01',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: 'i' },
+      { text: '오렌지색', value: 'e' }
+    ]
+  },
+  {
+    number: '02',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: 's' },
+      { text: '오렌지색', value: 'n' }
+    ]
+  },
+  {
+    number: '03',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: 'f' },
+      { text: '오렌지색', value: 't' }
+    ]
+  },
+  {
+    number: '04',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색!', value: 'j' },
+      { text: '오렌지색', value: 'p' }
+    ]
+  },
+  {
+    number: '05',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: '' },
+      { text: '오렌지색', value: '' }
+    ]
+  },
+  {
+    number: '06',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: '' },
+      { text: '오렌지색', value: '' }
+    ]
+  },
+  {
+    number: '07',
+    question: '나한테 어울린다고 생각하는 컬러는??',
+    choices: [
+      { text: '핑크색', value: '' },
+      { text: '오렌지색', value: '' }
+    ]
+  },
+  {
+    number: '08',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: '' },
+      { text: '오렌지색', value: '' }
+    ]
+  },
+  {
+    number: '09',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: '' },
+      { text: '오렌지색', value: '' }
+    ]
+  },
+  {
+    number: '10',
+    question: '나한테 어울린다고 생각하는 컬러는?',
+    choices: [
+      { text: '핑크색', value: '' },
+      { text: '오렌지색', value: '' }
+    ]
+  },
+]
+export const results = [
+  {
+    title: 'MUTE PINK',
+    character: '/images/result_character1.png',
+    results: [
+      '베이비 핑크에 베이지 한 방울이 들어가 차분하면서도, <br> 특유의 은은함으로 전체 혈색을 살려주는 컬러로 <br> 자연스러운 발색에 도움이 됩니다.'
+    ],
+    lipstickImg: '/images/result_lecture1.png',
+    llipstickUrl: 'https://www.shilladfs.com/estore/kr/ko/p/5086269'
+  },
+  {
+    title: 'MUTE PINK',
+    character: '/images/result_character2.png',
+    results: [
+      '베이비 핑크에 베이지 한 방울이 들어가 차분하면서도, <br> 특유의 은은함으로 전체 혈색을 살려주는 컬러로 <br> 자연스러운 발색에 도움이 됩니다.'
+    ],
+    llipstickImg: '/images/result_lecture2.png',
+    lipstickUrl: 'https://www.shilladfs.com/estore/kr/ko/p/5086269'
+  },
+  {
+    title: 'MUTE PINK',
+    character: '/images/result_character3.png',
+    results: [
+      '베이비 핑크에 베이지 한 방울이 들어가 차분하면서도, <br> 특유의 은은함으로 전체 혈색을 살려주는 컬러로 <br> 자연스러운 발색에 도움이 됩니다.'
+    ],
+    lipstickImg: '/images/result_lecture3.png',
+    lipstickUrl: 'https://www.shilladfs.com/estore/kr/ko/p/5086269'
+  },
+  {
+    title: 'MUTE PINK',
+    character: '/images/result_character4.png',
+    results: [
+      '베이비 핑크에 베이지 한 방울이 들어가 차분하면서도, <br> 특유의 은은함으로 전체 혈색을 살려주는 컬러로 <br> 자연스러운 발색에 도움이 됩니다.'
+    ],
+    lipstickImg: '/images/result_lecture4.png',
+    lipstickUrl: 'https://www.shilladfs.com/estore/kr/ko/p/5086269'
+  },
+  {
+    title: 'MUTE PINK',
+    character: '/images/result_character5.png',
+    results: [
+      '베이비 핑크에 베이지 한 방울이 들어가 차분하면서도, <br> 특유의 은은함으로 전체 혈색을 살려주는 컬러로 <br> 자연스러운 발색에 도움이 됩니다.'
+    ],
+    lipstickImg: '/images/result_lecture5.png',
+    lipstickUrl: 'https://www.shilladfs.com/estore/kr/ko/p/5086269'
+  },
+  {
+    title: 'MUTE PINK',
+    character: '/images/result_character6.png',
+    results: [
+      '베이비 핑크에 베이지 한 방울이 들어가 차분하면서도, <br> 특유의 은은함으로 전체 혈색을 살려주는 컬러'
+    ],
+    lipstickImg: '/images/result_lecture6.png',
+    lipstickUrl: 'https://www.shilladfs.com/estore/kr/ko/p/5086269'
+  }
+]
+export const mbtis = {
+  entj: 0,
+  entp: 0,
+  estp: 0,
+  esfp: 1,
+  esfj: 1,
+  enfj: 1,
+  infp: 2,
+  isfp: 2,
+  isfj: 2,
+  infj: 2,
+  estj: 3,
+  istp: 3,
+  intj: 4,
+  intp: 4,
+  istj: 4,
+  enfp: 5
+}
